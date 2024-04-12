0374f1c23a1adce27f77f2bed7d68f8acb251bb1
ManasArs13 <manasyan.a.s@ya.ru>
2024-04-12 19:02:10 +0300

    Update OrderAmoOrder.php

--- app/Models/OrderAmoOrder.php
+++ app/Models/OrderAmoOrder.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 class OrderAmoOrder extends Model
 {
@@ -13,13 +14,29 @@ class OrderAmoOrder extends Model
         "*"
     ];
 
-    public function orderMs()
-    {
-        return $this->belongsTo(Order::class );
-    }
+    // public function orderMs()
+    // {
+    //     return $this->belongsTo(Order::class );
+    // }
 
     public function orderAmo()
     {
         return $this->belongsTo(OrderAmo::class );
+    }
+
+
+    public function contact_amo()
+    {
+        return $this->belongsTo(ContactAmo::class);
+    }
+
+    public function status_amo()
+    {
+        return $this->belongsTo(StatusAmo::class);
+    }
+
+    public function orderMs():HasOneThrough
+    {
+        return $this->hasOneThrough(Order::class, OrderAmo::class, 'order_amo_id', 'id', 'id', 'order_id');
     }
 }
