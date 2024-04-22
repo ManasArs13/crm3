6b147e2ca00f511a041d338c1107b75ccb77c1a7
ManasArs13 <manasyan.a.s@ya.ru>
2024-04-22 08:42:13 +0300

    Update index.blade.php

--- resources/views/shipment/index.blade.php
+++ resources/views/shipment/index.blade.php
@@ -208,9 +208,7 @@
                                     </th>
                                 @endif
                             @endforeach
-                            @if (isset($needMenuForItem) && $needMenuForItem)
                                 <th></th>
-                            @endif
                         </tr>
                     </thead>
                     <tbody>
@@ -281,37 +279,27 @@
                                     </td>
                                 @endforeach
 
-                                @if (isset($needMenuForItem) && $needMenuForItem)
-                                    <td class=" text-nowrap px-6 py-4">
-                                        @if (isset($urlShow) && $urlShow != '')
-                                            <a class="bg-green-400 p-2 rounded-l font-semibold hover:bg-green-500 hover:text-white hover:border-y-2 border-green-500"
-                                                href="{{ route($urlShow, $entityItem->id) }}">
-                                                <i class="fas fa-folder">
-                                                </i>
-                                                {{ __('label.view') }}
-                                            </a>
-                                        @endif
-                                        @if (isset($urlEdit) && $urlEdit != '')
-                                            <a class="bg-blue-400 p-2 font-semibold hover:bg-blue-500 hover:text-white  hover:border-y-2 border-blue-500"
-                                                href="{{ route($urlEdit, $entityItem->id) }}">
-                                                {{ __('label.edit') }}
-                                            </a>
-                                        @endif
-                                        @if (isset($urlDelete) && $urlDelete != '')
-                                            <form action="{{ route($urlDelete, $entityItem->id) }}" method="Post"
-                                                class="inline">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit"
-                                                    class="bg-red-400 rounded-r font-semibold hover:bg-red-500 hover:text-white border-red-500"
-                                                    style="padding: 0.4rem;"
-                                                    href="{{ route($urlDelete, $entityItem->id) }}">
-                                                    {{ __('label.delete') }}
-                                                </button>
-                                            </form>
-                                        @endif
-                                    </td>
-                                @endif
+                                {{-- Delete --}}
+                                <td class="text-nowrap px-6 py-4">
+
+                                    <form action="{{ route($urlDelete, $entityItem->id) }}" method="Post"
+                                        class="inline">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit"
+                                            class="rounded-lg p-1 font-semibold hover:bg-red-500 hover:text-white border border-red-500"
+                                            href="{{ route($urlDelete, $entityItem->id) }}">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                class="w-6 h-6 stroke-red-500 hover:stroke-white">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                            </svg>
+
+                                        </button>
+                                    </form>
+                                </td>
+
                             </tr>
                         @endforeach
                     </tbody>
