3ba461febf421838678f42c7bc914a185fad8888
Smyluck <veb-master.veb@yandex.ru>
2024-06-15 07:48:20 +0300

    Сводка:: выгрузка остатка

--- app/Http/Controllers/SummaryController.php
+++ app/Http/Controllers/SummaryController.php
@@ -10,8 +10,8 @@ use Illuminate\Support\Facades\DB;
 class SummaryController extends Controller
 {
     public function index(){
-        $sumMaterials=Product::where("category_id", 8)->sum(DB::raw('balance * price'));
-        $sumProducts=Product::whereIn("category_id", [4,5,6,7,11,12,15,16,21])->sum(DB::raw('balance * price'));
+        $sumMaterials=Product::where("category_id", 8)->sum(DB::raw('residual * price'));
+        $sumProducts=Product::whereIn("category_id", [4,5,6,7,11,12,15,16,21])->sum(DB::raw('residual * price'));
         $sumMutualSettlement=Contact::whereNot("balance",NULL)->sum("balance");
         $sumMutualSettlementMain=Contact::whereHas('contact_categories', function($q) {
             $q->where('contact_category_id', '=', '8');
