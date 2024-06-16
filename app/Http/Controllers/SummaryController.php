e488d1d58c6396da3c56ab9df14b43b3409c56e1
Smyluck <veb-master.veb@yandex.ru>
2024-06-16 13:54:10 +0300

    Сводка: без категории бетон.

--- app/Http/Controllers/SummaryController.php
+++ app/Http/Controllers/SummaryController.php
@@ -11,7 +11,7 @@ class SummaryController extends Controller
 {
     public function index(){
         $sumMaterials=Product::where("category_id", 8)->sum(DB::raw('residual * price'));
-        $sumProducts=Product::whereIn("category_id", [4,5,6,7,11,12,15,16,21])->sum(DB::raw('residual * price'));
+        $sumProducts=Product::whereIn("category_id", [5,6,7,11,12,15,16,21])->sum(DB::raw('residual * price'));
         $sumMutualSettlement=Contact::whereNot("balance",NULL)->sum("balance");
         $sumMutualSettlementMain=Contact::whereHas('contact_categories', function($q) {
             $q->where('contact_category_id', '=', '8');
