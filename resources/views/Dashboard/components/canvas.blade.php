453c2123c381ed2d7738972bbeb60dba378b717d
ManasArs13 <manasyan.a.s@ya.ru>
2024-06-25 13:47:28 +0300

    Update canvas.blade.php

--- resources/views/Dashboard/components/canvas.blade.php
+++ resources/views/Dashboard/components/canvas.blade.php
@@ -1,16 +1,24 @@
 <div class="border-t-2 p-5">
     <div class="chart-container" style="height:20vh;">
-        <canvas  id="orderChart"></canvas>
+        <canvas id="orderChart"></canvas>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
-        window.addEventListener("DOMContentLoaded",async function () {
-            
-            let sum  = [];
+        window.addEventListener("DOMContentLoaded", async function() {
+
+            let sum = [];
             let orders_count = [];
             let positions_count = [];
             let shipped_count = [];
             let labels = [];
+
+            function count(arr) {
+                let sum = 0;
+                arr.forEach(function(item) {
+                    sum += item
+                })
+                return sum;
+            }
 
             await fetch('/get-orders/{!! $date !!}')
                 .then((response) => response.json())
@@ -34,7 +42,7 @@
                     for (let item in data.shipped_count) {
                         shipped_count.push(data.shipped_count[item])
                     }
-                    
+
                 })
                 .catch((error) => {
                     console.error("Error fetching data:", error);
@@ -51,9 +59,8 @@
                 type: "line",
                 data: {
                     labels: labels,
-                    datasets: [
-                        {
-                            label: "Сумма заказов",
+                    datasets: [{
+                            label: "Сумма заказов " + ' (' + count(sum) + ' р.)',
                             hidden: true,
                             data: sum,
                             backgroundColor: "rgb(145,202,246)",
@@ -61,39 +68,39 @@
                             borderWidth: 4,
                         },
                         {
-                            label: "Кол-во заказов",
+                            label: "Кол-во заказов" + ' (' + count(orders_count) + ' шт.)',
                             data: orders_count,
                             hidden: true,
                             backgroundColor: "rgb(236,112,112)",
-                            borderColor:  "rgb(236,112,112)",
+                            borderColor: "rgb(236,112,112)",
                             borderWidth: 4,
                         },
                         {
-                            label: "Кол-во продуктов",
+                            label: "Кол-во продуктов" + ' (' + count(positions_count) + ' шт.)',
                             data: positions_count,
                             backgroundColor: "rgb(255, 205, 86)",
-                            borderColor:  "rgb(255, 205, 86)",
+                            borderColor: "rgb(255, 205, 86)",
                             borderWidth: 4,
                         },
                         {
-                            label: "Кол-во отгружено",
+                            label: "Кол-во отгружено" + ' (' + count(shipped_count) + ' шт.)',
                             data: shipped_count,
                             backgroundColor: "rgb(134 239 172)",
-                            borderColor:  "rgb(134 239 172)",
+                            borderColor: "rgb(134 239 172)",
                             borderWidth: 4,
                         }
                     ],
                 },
                 options: {
                     scale: {
-                        ticks:{
+                        ticks: {
                             beginAtZero: true,
                             max: 4
                         }
                     },
                     interaction: {
                         mode: 'index'
-                    } ,
+                    },
                     responsive: true,
                     maintainAspectRatio: false,
                     width: 400,
