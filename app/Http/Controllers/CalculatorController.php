00ef61ef4a92c293e02fe0599a7b55b4cce13933
ManasArs13 <manasyan.a.s@ya.ru>
2024-08-15 18:29:32 +0300

    Update CalculatorController.php

--- app/Http/Controllers/CalculatorController.php
+++ app/Http/Controllers/CalculatorController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Delivery;
 use App\Models\Product;
-use App\Models\ShipingPrice;
 use App\Models\Shipment;
 use App\Models\TransportType;
 use App\Models\Date;
