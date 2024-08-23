5b5aa189b5971b0b3ccabce90ee5e82a264219aa
ManasArs13 <manasyan.a.s@ya.ru>
2024-08-23 14:35:10 +0300

    Update OrderMsService.php

--- app/Services/EntityMs/OrderMsService.php
+++ app/Services/EntityMs/OrderMsService.php
@@ -6,6 +6,7 @@ use App\Models\Delivery;
 use App\Models\Option;
 use App\Services\Api\MoySkladService;
 use App\Services\EntityMs\CounterpartyMsService;
+use Illuminate\Support\Facades\Log;
 
 class OrderMsService
 {
@@ -13,114 +14,115 @@ class OrderMsService
     private $moySkladService;
     private $counterpartyMsService;
 
-    public function __construct(MoySkladService $moySkladService, CounterpartyMsService $counterpartyMsService){
-        $this->moySkladService=$moySkladService;
-        $this->counterpartyMsService=$counterpartyMsService;
+    public function __construct(MoySkladService $moySkladService, CounterpartyMsService $counterpartyMsService)
+    {
+        $this->moySkladService = $moySkladService;
+        $this->counterpartyMsService = $counterpartyMsService;
     }
 
     function updateOrderMs($msOrder)
     {
-        $url = Option::where('code', '=',"ms_orders_url")->first()?->value;
-        $urlProduct = Option::where('code', '=',"ms_product_for_order_url")->first()?->value;
-        $customentity= Option::where('code', '=',"ms_customentity_url")->first()?->value;
+        $url = Option::where('code', '=', "ms_orders_url")->first()?->value;
+        $urlProduct = Option::where('code', '=', "ms_product_for_order_url")->first()?->value;
+        $customentity = Option::where('code', '=', "ms_customentity_url")->first()?->value;
 
-        $urlCounterparty = Option::where('code', '=',"ms_counterparty_url")->first()?->value;
-        $urlDelivery = Option::where('code', '=',"ms_delivery_url")->first()?->value;
-        $urlVehicleCategory = Option::where('code', '=',"ms_vehicle_type_url")->first()?->value;
-        $urlService = Option::where('code', '=',"ms_service_url")->first()?->value;
+        $urlCounterparty = Option::where('code', '=', "ms_counterparty_url")->first()?->value;
+        $urlDelivery = Option::where('code', '=', "ms_delivery_url")->first()?->value;
+        $urlVehicleCategory = Option::where('code', '=', "ms_vehicle_type_url")->first()?->value;
+        $urlService = Option::where('code', '=', "ms_service_url")->first()?->value;
 
-        $urlState =Option::where('code', '=',"ms_orders_need_status_url")->first()?->value;
-        $urlAttr = Option::where('code', '=','ms_attributes_order_date_url')->first()?->value;
+        $urlState = Option::where('code', '=', "ms_orders_need_status_url")->first()?->value;
+        $urlAttr = Option::where('code', '=', 'ms_attributes_order_date_url')->first()?->value;
 
-        $guidAttrDelivery = Option::where('code', '=','ms_order_delivery_guid')->first()?->value;
-        $guidAttrDeliveryPrice = Option::where('code', '=','ms_order_delivery_price_guid')->first()?->value;
-        $guidAttrTransportType = Option::where('code', '=','ms_order_transport_type_guid')->first()?->value;
+        $guidAttrDelivery = Option::where('code', '=', 'ms_order_delivery_guid')->first()?->value;
+        $guidAttrDeliveryPrice = Option::where('code', '=', 'ms_order_delivery_price_guid')->first()?->value;
+        $guidAttrTransportType = Option::where('code', '=', 'ms_order_transport_type_guid')->first()?->value;
 
-        $urlOrganization = Option::where('code', '=',"ms_organization_url")->first()?->value;
-        $guidOrganization = Option::where('code', '=','ms_organization_guid')->first()?->value;
+        $urlOrganization = Option::where('code', '=', "ms_organization_url")->first()?->value;
+        $guidOrganization = Option::where('code', '=', 'ms_organization_guid')->first()?->value;
 
-        $vat = Option::where('code', '=',"ms_vat")->first()?->value;
-        $service_delivery_block = Option::where('code', '=',"ms_service_delivery_block_id")->first()?->value;
-        $service_delivery_beton = Option::where('code', '=',"ms_service_delivery_beton_id")->first()?->value;
+        $vat = Option::where('code', '=', "ms_vat")->first()?->value;
+        $service_delivery_block = Option::where('code', '=', "ms_service_delivery_block_id")->first()?->value;
+        $service_delivery_beton = Option::where('code', '=', "ms_service_delivery_beton_id")->first()?->value;
 
-        $array=[];
+        $array = [];
 
         if (isset($msOrder["positions"])) {
             $array["positions"] = [];
-            $quantity=1;
+            $quantity = 1;
 
             foreach ($msOrder["positions"] as $position) {
-                if ($position["quantity"]!=0){
+                if ($position["quantity"] != 0) {
                     $array["positions"][] = [
                         "quantity" => (float)$position["quantity"],
                         "price" => (float)$position["price"] * 100,
-                        'vat'=>(int)$vat,
+                        'vat' => (int)$vat,
                         "assortment" => [
                             "meta" => [
-                                "href" => $urlProduct.$position["product_id"],
+                                "href" => $urlProduct . $position["product_id"],
                                 "type" => "product",
                                 "mediaType" => "application/json"
                             ]
                         ]
                     ];
-                    $quantity=(float)$position["quantity"];
+                    $quantity = (float)$position["quantity"];
                 }
             }
 
-            if (count($array["positions"])==0)
+            if (count($array["positions"]) == 0)
                 throw new \Exception(trans("error.noPositions"));
 
-            $delivery_id=$service_delivery_block;
+            $delivery_id = $service_delivery_block;
 
 
-            if ($msOrder["form"]=="calcBeton"){
-                $delivery_id=$service_delivery_beton;
-            }else{
-                $quantity=1;
+            if ($msOrder["form"] == "calcBeton") {
+                $delivery_id = $service_delivery_beton;
+            } else {
+                $quantity = 1;
             }
-            $price=0;
+            $price = 0;
 
 
-            if (\Arr::exists($msOrder,"attributes") && \Arr::exists($msOrder["attributes"], "deliveryPrice")){
-                $price=(float)$msOrder["attributes"]["deliveryPrice"]*100;
+            if (\Arr::exists($msOrder, "attributes") && \Arr::exists($msOrder["attributes"], "deliveryPrice")) {
+                $price = (float)$msOrder["attributes"]["deliveryPrice"] * 100;
             }
 
 
             $array["positions"][] = [
-                    "quantity" => (float)$quantity,
-                    "price" =>(float)$price,
-                    'vat'=> (int)$vat,
-                    "assortment" => [
-                        "meta" => [
-                            "href" => $urlService.$delivery_id,
-                            "type" => "service",
-                            "mediaType" => "application/json"
-                        ]
+                "quantity" => (float)$quantity,
+                "price" => (float)$price,
+                'vat' => (int)$vat,
+                "assortment" => [
+                    "meta" => [
+                        "href" => $urlService . $delivery_id,
+                        "type" => "service",
+                        "mediaType" => "application/json"
                     ]
+                ]
             ];
         }
 
-        if (isset($msOrder["description"])){
-            $array["description"]=$msOrder["description"];
+        if (isset($msOrder["description"])) {
+            $array["description"] = $msOrder["description"];
         }
 
 
-        if (isset($msOrder["shipmentAddressFull"]) && isset($msOrder["shipmentAddressFull"]["comment"])){
-            $array["shipmentAddressFull"]["comment"]=$msOrder["shipmentAddressFull"]["comment"];
+        if (isset($msOrder["shipmentAddressFull"]) && isset($msOrder["shipmentAddressFull"]["comment"])) {
+            $array["shipmentAddressFull"]["comment"] = $msOrder["shipmentAddressFull"]["comment"];
         }
 
-        if (isset($msOrder["shipmentAddressFull"]) && isset($msOrder["shipmentAddressFull"]["addInfo"])){
-            $array["shipmentAddressFull"]["addInfo"]=$msOrder["shipmentAddressFull"]["addInfo"];
+        if (isset($msOrder["shipmentAddressFull"]) && isset($msOrder["shipmentAddressFull"]["addInfo"])) {
+            $array["shipmentAddressFull"]["addInfo"] = $msOrder["shipmentAddressFull"]["addInfo"];
         }
 
-        if (isset($msOrder["deliveryPlannedMoment"])){
-            $array["deliveryPlannedMoment"]=$msOrder["deliveryPlannedMoment"];
+        if (isset($msOrder["deliveryPlannedMoment"])) {
+            $array["deliveryPlannedMoment"] = $msOrder["deliveryPlannedMoment"];
         }
 
-        if (!isset($msOrder["id"]) || $msOrder["id"]==null) {
+        if (!isset($msOrder["id"]) || $msOrder["id"] == null) {
             $array["organization"] = [
                 "meta" => [
-                    "href" => $urlOrganization.$guidOrganization,
+                    "href" => $urlOrganization . $guidOrganization,
                     "type" => "organization",
                     "mediaType" => "application/json"
                 ]
@@ -128,11 +130,11 @@ class OrderMsService
         }
 
         if (isset($msOrder["agent"])) {
-            if (is_array($msOrder["agent"])){
-                if (isset($msOrder["agent"]["id"]) && $msOrder["agent"]["id"]!=null){
+            if (is_array($msOrder["agent"])) {
+                if (isset($msOrder["agent"]["id"]) && $msOrder["agent"]["id"] != null) {
                     $msOrder["agent"] = $msOrder["agent"]["id"];
-                }else{
-                    $agent=$this->counterpartyMsService->updateCounterpartyMs($msOrder["agent"]);
+                } else {
+                    $agent = $this->counterpartyMsService->updateCounterpartyMs($msOrder["agent"]);
                     $msOrder["agent"] = $agent->id;
                 }
             }
@@ -147,7 +149,7 @@ class OrderMsService
         }
 
 
-        if (isset($msOrder["state"]) && $msOrder["state"]!=null){
+        if (isset($msOrder["state"]) && $msOrder["state"] != null) {
             $array["state"]  = [
                 'meta' => [
                     'href' => $urlState . $msOrder["state"],
@@ -157,9 +159,9 @@ class OrderMsService
             ];
         }
 
-        if (\Arr::exists($msOrder,"attributes")){
+        if (\Arr::exists($msOrder, "attributes")) {
             if (\Arr::exists($msOrder["attributes"], "deliveryPrice"))
-                $array["attributes"][]=[
+                $array["attributes"][] = [
                     'meta' => [
                         'href' => $urlAttr . $guidAttrDeliveryPrice,
                         'type' => "attributemetadata",
@@ -169,7 +171,7 @@ class OrderMsService
                 ];
 
             if (\Arr::exists($msOrder["attributes"], "delivery"))
-                $array["attributes"][]=[
+                $array["attributes"][] = [
                     'meta' => [
                         'href' => $urlAttr . $guidAttrDelivery,
                         'type' => "attributemetadata",
@@ -183,13 +185,13 @@ class OrderMsService
                         ],
                         // "name"=>$msOrder["attributes"]["delivery"]["name"]
                     ],
-                    "id"=> $guidAttrDelivery,
-                    "type"=> "customentity"
+                    "id" => $guidAttrDelivery,
+                    "type" => "customentity"
                 ];
 
 
             if (\Arr::exists($msOrder["attributes"], "vehicle_type"))
-                $array["attributes"][]=[
+                $array["attributes"][] = [
                     'meta' => [
                         'href' => $urlAttr . $guidAttrTransportType,
                         'type' => "attributemetadata",
@@ -203,15 +205,23 @@ class OrderMsService
                         ],
                         // "name"=>$msOrder["attributes"]["vehicle_type"]["name"]
                     ],
-                    "id"=> $guidAttrTransportType,
-                    "type"=> "customentity"
+                    "id" => $guidAttrTransportType,
+                    "type" => "customentity"
                 ];
         }
-
-        if (!isset($msOrder["id"]) || $msOrder["id"]==null) {
+        log($array);
+        if (!isset($msOrder["id"]) || $msOrder["id"] == null) {
             return $this->moySkladService->actionPostRowsFromJson($url, $array);
-        }else{
-            return $this->moySkladService->actionPutRowsFromJson($url.$msOrder["guid"], $array);
+        } else {
+            return $this->moySkladService->actionPutRowsFromJson($url . $msOrder["guid"], $array);
         }
+    }
+
+    function createOrderMs($msOrder) 
+    {
+        $url = Option::where('code', '=', "ms_orders_url")->first()?->value;
+        $array = '';
+
+        $this->moySkladService->actionPostRowsFromJson($url, $array);
     }
 }
