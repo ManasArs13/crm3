b69ab7c654286b2d85025b69a67f87fe9f0cba98
Davit <davitashotyan9@gmail.com>
2024-09-12 16:35:28 +0400

    Ненужные фильтры в отгрузках и в заказах удалены

--- resources/views/shipment/index.blade.php
+++ resources/views/shipment/index.blade.php
@@ -174,64 +174,6 @@
                         </div>
                     </form>
 
-                    {{-- Query Filters --}}
-                    <div class="flex flex-row gap-1">
-                        <form method="get" action="{{ route($urlFilter) }}" class="flex gap-1">
-                            <div class="flex flex-row gap-1">
-                                @foreach ($filters as $filter)
-                                    @if ($filter['name'] == 'status')
-                                        <div class="flex flex-row gap-1 w-100">
-                                            <div>
-                                                <select
-                                                    class="border border-solid border-neutral-300 rounded w-full py-2 mb-4"
-                                                    , name="filters[{{ $filter['name'] }}]" data-offset="false">
-                                                    @foreach ($filter['values'] as $value)
-                                                        <option @if ($value['value'] == $filter['checked_value']) selected @endif
-                                                            value="{{ $value['value'] }} ">
-                                                            {{ $value['name'] }}</option>
-                                                    @endforeach
-                                                </select>
-                                            </div>
-                                        </div>
-                                    @elseif ($filter['name'] == 'transport')
-                                        <div class="flex flex-row gap-1 w-100">
-                                            <div>
-                                                <select
-                                                    class="border border-solid border-neutral-300 rounded w-full py-2 mb-4"
-                                                    , name="filters[{{ $filter['name'] }}]" data-offset="false">
-                                                    @foreach ($filter['values'] as $value)
-                                                        <option @if ($value['value'] == $filter['checked_value']) selected @endif
-                                                            value="{{ $value['value'] }} ">
-                                                            {{ $value['name'] }}</option>
-                                                    @endforeach
-                                                </select>
-                                            </div>
-                                        </div>
-                                    @elseif ($filter['name'] == 'delivery')
-                                        <div class="flex flex-row gap-1 w-100">
-                                            <div>
-                                                <select
-                                                    class="border border-solid border-neutral-300 rounded w-full py-2 mb-4"
-                                                    , name="filters[{{ $filter['name'] }}]" data-offset="false">
-                                                    @foreach ($filter['values'] as $value)
-                                                        <option @if ($value['value'] == $filter['checked_value']) selected @endif
-                                                            value="{{ $value['value'] }} ">
-                                                            {{ $value['name'] }}</option>
-                                                    @endforeach
-                                                </select>
-                                            </div>
-                                        </div>
-                                    @endif
-                                @endforeach
-                            </div>
-                            <div>
-                                <button type="submit"
-                                    class="inline-flex rounded bg-blue-600 border-2 border-blue-600 px-4 py-2 text-md font-medium leading-normal text-white hover:bg-blue-700">
-                                    отфильтровать
-                                </button>
-                            </div>
-                        </form>
-                    </div>
 
                     @if (isset($urlCreate) && $urlCreate != '')
                         <div class="flex px-3 text-center font-bold">
