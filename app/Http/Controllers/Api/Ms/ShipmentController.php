e209c6bd8c43deb151803b40f54f77131765b312
Smyluck <veb-master.veb@yandex.ru>
2024-09-18 10:01:25 +0300

    Фикс 2:  Отгрузка: создание кнопки создать огрузку в мс

--- app/Http/Controllers/Api/Ms/ShipmentController.php
+++ app/Http/Controllers/Api/Ms/ShipmentController.php
@@ -16,13 +16,19 @@ class ShipmentController extends Controller
         $id=$request->id;
 
         $result = $shipmentMsService->createChipmentToMs($id);
+        $shipment=Shipment::find($id);
+
+        $text="cоздана";
+        if ($shipment->ms_id!=null){
+            $text="редактирована";
+        }
 
         if (isset($result->id)) {
             $shipment=Shipment::find($id);
             $shipment->ms_id=$result->id;
             $shipment->save();
 
-            return new Response("<a href='" . 'https://online.moysklad.ru/app/#demand/edit?id=' . $result->id . "' class='font-medium text-blue-600 dark:text-blue-500 hover:underline'  target='_blank'>" . 'Отгрузка №' . $result->name . ' создан!' . "</a>", 200);
+            return new Response("<a href='" . 'https://online.moysklad.ru/app/#demand/edit?id=' . $result->id . "' class='font-medium text-blue-600 dark:text-blue-500 hover:underline'  target='_blank'>" . 'Отгрузка №' . $result->name . $text . " !</a>", 200);
         } else {
             return new Response(trans("error.Error").$result);
         }
