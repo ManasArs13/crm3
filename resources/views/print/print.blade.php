925fbd51c84c5be34bbcc3795d41c2baf71f34f6
Davit <davitashotyan9@gmail.com>
2024-09-20 15:51:25 +0400

    Добавлен принт в заказах и в отгрузках

--- resources/views/print/print.blade.php
+++ resources/views/print/print.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <style>
 
 
@@ -10,7 +13,9 @@
         }
 
         body {
-            font-family: Arial, sans-serif;
+            font-family: "Open Sans", sans-serif;
+            font-optical-sizing: auto;
+            font-style: normal;
             font-size: 12pt;
             margin: 0;
             padding: 0;
@@ -41,9 +46,13 @@
         }
 
         th, td {
-            border: 2px solid #000;
+            border: 0.4mm solid #000;
             padding: 8px;
             text-align: left;
+        }
+
+        th{
+            font-weight: 600;
         }
 
         .no-print {
@@ -66,15 +75,15 @@
     <div class="content">
         <div style="margin-top: 15px;">
             <div style="float: left;">Поставщик</div>
-            <div style="float: left;margin-left: 43px;font-weight: bold;">ООО Трест Траст Асбест Плюс</div>
+            <div style="float: left;margin-left: 43px;font-weight: 600;">ООО ЕВРОГРУПП</div>
             <br>
-            <div style="margin-top: 5px;">(Исполнитель):</div>
+            <div>(Исполнитель):</div>
         </div>
         <div style="margin-top: 19px;">
             <div style="float: left;">Покупатель</div>
-            <div style="float: left;margin-left: 43px;font-weight: bold;">@if(isset($order)) {{ $order->contact->name }} @elseif(isset($shipment)) {{ $shipment->contact->name }} @endif</div>
+            <div style="float: left;margin-left: 43px;font-weight: 600;">@if(isset($order)) {{ $order->contact->name }} @elseif(isset($shipment)) {{ $shipment->contact->name }} @endif</div>
             <br>
-            <div style="margin-top: 5px;">(Заказчик):</div>
+            <div>(Заказчик):</div>
         </div>
         <table style="margin-top: 7px;">
 
@@ -116,9 +125,20 @@
             </tbody>
 
         </table>
-        <div style="float: right;font-size: 17px;font-weight: bold;margin-top: 10px;">
+        <div style="float: right;font-size: 17px;font-weight: 600;margin-top: 10px;">
             <div style="float: right;">@if(isset($order)) {{ $order->sum }} @elseif(isset($shipment)) {{ $totalSuma }} @endif</div>
             <div style="float: right;margin-right: 15px;">Итого:</div>
+        </div>
+        <div style="width: 100%;height: 30mm;margin-top: 50mm;">
+            <div style="width: 55mm;float: left;">
+                <div style="float: left;border-bottom: 0.7mm solid #000;width: 100%;"></div>
+                <div style="font-weight: 600;">Поставщик</div>
+            </div>
+            <div style="width: 55mm;float: right;">
+                <div style="float: right;border-bottom: 0.7mm solid #000;width: 100%;"></div>
+                <div style="font-weight: 600;float: right;">Заказчик</div>
+            </div>
+
         </div>
     </div>
 </div>
