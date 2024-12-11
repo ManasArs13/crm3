5b4edcf1315dd54797ff68d5aade1dd218c3ad4f
Smyluck <veb-master.veb@yandex.ru>
2024-12-11 14:08:58 +0300

    Таб Деливери: добавление колонок

---
+++ database/migrations/2024_12_11_140457_add_platas_to_deliveries_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('deliveries', function (Blueprint $table) {
+            $table->integer("plata_m");
+            $table->integer("plata_b");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('deliveries', function (Blueprint $table) {
+            $table->dropColumn("plata_m");
+            $table->dropColumn("plata_b");
+        });
+    }
+};
